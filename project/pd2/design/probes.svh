c474736fee136e8cfe5c2c0b00cf714c1e088f61
abdulbasitali02 <103627220+abdulbasitali02@users.noreply.github.com>
2025-10-12 22:49:27 -0400

    Integrate fetch, decode, and control stages in pd2
    Added instantiations and signal wiring for fetch, decode, and control modules in pd2.sv, including memory interface and internal signal definitions. Updated probes.svh to map probe macros to corresponding internal signals for easier debugging and observation.

--- project/pd2/design/probes.svh
+++ project/pd2/design/probes.svh
@@ -1,16 +1,16 @@
 // ----  Probes  ----
-//`define PROBE_F_PC // ??
-//`define PROBE_F_INSN // ??
+`define PROBE_F_PC if_pc
+`define PROBE_F_INSN if_insn
 
-//`define PROBE_D_PC // ??
-//`define PROBE_D_OPCODE // ??
-//`define PROBE_D_RD // ??
-//`define PROBE_D_FUNCT3 // ??
-//`define PROBE_D_RS1 // ??
-//`define PROBE_D_RS2 // ??
-//`define PROBE_D_FUNCT7 // ??
-//`define PROBE_D_IMM // ??
-//`define PROBE_D_SHAMT // ??
+`define PROBE_D_PC id_pc
+`define PROBE_D_OPCODE id_opcode
+`define PROBE_D_RD id_rd
+`define PROBE_D_FUNCT3 id_funct3
+`define PROBE_D_RS1 id_rs1
+`define PROBE_D_RS2 id_rs2
+`define PROBE_D_FUNCT7 id_funct7
+`define PROBE_D_IMM id_imm
+`define PROBE_D_SHAMT id_shamt
 // ----  Probes  ----
 
 // ----  Top module  ----
